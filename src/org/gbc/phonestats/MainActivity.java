fca9c82668bd0895fd45fc5d6fae960fbef10d01
Yuhao93 <johndoe@example.com>
2013-03-07 16:34:17 -0800

    Changed MainActivity a bit

--- src/org/gbc/phonestats/MainActivity.java
+++ src/org/gbc/phonestats/MainActivity.java
@@ -16,67 +16,63 @@ public class MainActivity extends Activity {
   private LocationHandler locationHandler = LocationHandler.getInstance();
   private OrientationHandler orientationHandler = OrientationHandler.getInstance();
 	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 		
-		// Start measurment handlers
-		locationHandler.start(this);
-		orientationHandler.start(this);
+    // Start measurment handlers
+    locationHandler.start(this);
+    orientationHandler.start(this);
 		
-		// Run the Socket Server on a background thread
-		(new Thread(
-			new Runnable(){
-				@Override
-				public void run(){
-					try{
+    // Run the Socket Server on a background thread
+    (new Thread(
+        new Runnable(){
+          @Override
+          public void run(){
+            try{
 					  
-					  
-					  // Create a server socket listening on port 8080
-						ServerSocket welcomeSocket = new ServerSocket(8080);
-						while(true){
+              // Create a server socket listening on port 8080
+              ServerSocket welcomeSocket = new ServerSocket(8080);
+              while(true){
 						  
-						  // Take in one socket at a time
-							Socket connectionSocket = welcomeSocket.accept();
+                // Take in one socket at a time
+                Socket connectionSocket = welcomeSocket.accept();
 							
-							// Read in the input from a client
-							// Once we've read a line of input, parse it
-							BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-							String res = inFromClient.readLine();
-							
-							System.out.println(res.trim().length() + " " + res + " " + res.trim().charAt(0) + " " + res.trim().charAt(res.trim().length() - 1) + "B");
-							
-							// If we are getting the location
-							if(res.trim().equals("get-location")){
-								DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-								outToClient.writeBytes(locationHandler.getMostRecentMeasurement() + '\n');
+                // Read in the input from a client
+                // Once we've read a line of input, parse it
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                String res = inFromClient.readLine();
+
+                // If we are getting the location
+                if(res.trim().equals("get-location")){
+                  DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                  outToClient.writeBytes(locationHandler.getMostRecentMeasurement() + '\n');
 								
 								
-							// If we are getting the orientation
-							}else if(res.trim().equals("get-orientation") || (res.length() > 0 && res.substring(1).trim().equals("get-orientation"))){
-							  DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-							  System.out.println(orientationHandler.getMostRecentMeasurement());
-							  outToClient.writeBytes(orientationHandler.getMostRecentMeasurement() + '\n');
+                // If we are getting the orientation
+                }else if(res.trim().equals("get-orientation") || (res.length() > 0 && res.substring(1).trim().equals("get-orientation"))){
+                  DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                  System.out.println(orientationHandler.getMostRecentMeasurement());
+                  outToClient.writeBytes(orientationHandler.getMostRecentMeasurement() + '\n');
 							  
-							  
-							// Unknown command returns empty string
-							}else{
-							  DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-                outToClient.writeBytes("\n");
-							}
-						}
-					}catch(IOException e){
-						e.printStackTrace();
-					}
-				}
-			}
-		)).start();
-	}
+                // Unknown command returns empty string
+                }else{
+                  DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                  outToClient.writeBytes("\n");
+                }
+              }
+            }catch(IOException e){
+              e.printStackTrace();
+            }
+          }
+        }
+    )).start();
+  }
 	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.activity_main, menu);
+    return true;
+  }
 }
